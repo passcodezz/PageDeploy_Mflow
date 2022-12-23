1681e37148e99b650f3bc1f9ddbba305618bb68f
konklom <siwakorn.swkk@gmail.com>
2022-12-23 13:32:44 +0700

    firstdeploy

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Lottie from "lottie-react";
 import Process from "./process.json"
-import Background from "./background.json"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <Lottie
                           className="lg:w-1/4 m-8 sm:m-10 lg:m-5"
-                          // className="w-20 h-20 -my-10 "
                           loop={true}
                           animationData={Process}
                         />
@@ -19,21 +16,6 @@ function App() {
         <p >
           เนื่องจากจะมีการปิดปรับปรุงระบบเพื่อเพิ่มประสิทธิภาพการให้บริการ
         </p>
-        {/* <p>
-          เพื่อเพิ่มประสิทธิภาพการให้บริการ
-        </p> */}
-       {/* <Lottie
-            loop={false}
-            animationData={Background}
-            // title="konklom"
-            style={{
-          
-              width: "40%",
-              zIndex: 1,
-              overflow: "hidden",
-              position: "fixed",
-            }}
-          /> */}
       </header>
     </div>
   );
